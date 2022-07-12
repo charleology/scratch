9398dc91bdccb8e22f30806764423fc8c59a8690
charleology <cnbelano@gmail.com>
2022-07-12 20:01:37 +0800

    Commitment is a no no.

--- app/src/main/java/com/example/scratch/SignUp.java
+++ app/src/main/java/com/example/scratch/SignUp.java
@@ -2,23 +2,45 @@ package com.example.scratch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.Calendar;
 
 public class SignUp extends AppCompatActivity {
 
     ImageView backButton;
+    EditText fieldDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
         getSupportActionBar().hide();
         backButton = (ImageView) findViewById(R.id.btnBack);
+        fieldDate = (EditText) findViewById(R.id.tfsignupBirthday);
+
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        MaterialDatePicker datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Select date").setSelection(MaterialDatePicker.todayInUtcMilliseconds()).build();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +51,17 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        fieldDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.show(getSupportFragmentManager(), "Material_Date_Picker");
+                datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+                        fieldDate.setText(datePicker.getHeaderText());
+                    }
+                });
+            }
+        });
     }
 }
