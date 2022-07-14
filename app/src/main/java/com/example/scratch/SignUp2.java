da796c6c97c4c77dc6e1735cf13b83ce4aad8009
charleology <cnbelano@gmail.com>
2022-07-14 14:56:25 +0800

    Commitment is a no no.

--- app/src/main/java/com/example/scratch/SignUp2.java
+++ app/src/main/java/com/example/scratch/SignUp2.java
@@ -3,19 +3,26 @@ package com.example.scratch;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import java.io.BufferedInputStream;
+import java.util.Arrays;
 
 public class SignUp2 extends AppCompatActivity {
 
@@ -48,16 +55,22 @@ public class SignUp2 extends AppCompatActivity {
         spinProvince.setText("Bulacan");
         etZip.setText("3023");
 
-        String[] brgy = getResources().getStringArray(R.array.Barangay);
+        //String[] brgy = getResources().getStringArray(R.array.Barangay);
 
-        ArrayAdapter<String> barangayAdapter = new ArrayAdapter<String>(this, R.layout.drop_down_items, brgy) {
+        String[] first = getResources().getStringArray(R.array.Gender);
+        String[] second = getResources().getStringArray(R.array.Barangay);
+
+        String[] both = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, both, first.length, second.length);
+
+        ArrayAdapter<String> barangayAdapter = new ArrayAdapter<String>(this, R.layout.drop_down_items, both) {
             @Override
             public int getCount() {
-                return brgy.length-1;
+                return both.length-1;
             }
         };
         spinBarangay.setAdapter(barangayAdapter);
-        spinBarangay.setSelection(brgy.length-1);
+        spinBarangay.setSelection(both.length-1);
 
         ivPicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +86,49 @@ public class SignUp2 extends AppCompatActivity {
 
                 startActivityForResult(chooserIntent, PICK_IMAGE);
             }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(SignUp2.this);
+                builder.setTitle("Terms and Conditions");
+                builder.setIcon(getDrawable(R.drawable.circlelogo_gradient));
+                builder.setBackground(getDrawable(R.drawable.dialog_bg));
+                builder.setMessage(getResources().getString(R.string.msg));
+
+                LinearLayout tnc_layout = new LinearLayout(SignUp2.this);
+                tnc_layout.setOrientation(LinearLayout.VERTICAL);
+                tnc_layout.setVerticalGravity(10);
+                TextView tnc_tv = new TextView(SignUp2.this);
+                tnc_tv.setText("Terms and Conditions");
+                tnc_tv.setTextColor(getColor(R.color.purple_700));
+                tnc_tv.setPadding(60,0,0,0);
+                tnc_layout.addView(tnc_tv);
+                builder.setView(tnc_layout);
+
+                builder.setPositiveButton("SUBMIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Codes here
+                    }
+                });
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //codes here
+                    }
+                });
+                builder.show();
+
+                tnc_tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showTnc();
+                    }
+                });
+            }
+
         });
     }
 
@@ -100,5 +156,10 @@ public class SignUp2 extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         this.finish();
+    }
+
+    public void showTnc(){
+        Intent i = new Intent(SignUp2.this, TermsConditions.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/scratch/TermsConditions.java
@@ -0,0 +1,26 @@
+package com.example.scratch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TermsConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_conditions);
+        getSupportActionBar().hide();
+    }
+
+    public void back(View view){
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+}
