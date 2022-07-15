e415dd3835a93cbf368392aef010a56ae8fa89ec
charleology <cnbelano@gmail.com>
2022-07-15 21:51:47 +0800

    Commitment is a no no.

--- app/src/main/java/com/example/scratch/MainActivity.java
+++ app/src/main/java/com/example/scratch/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.scratch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button signUp, logIn;
+    TextView forgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         signUp = (Button) findViewById(R.id.btnSignup);
         logIn = (Button) findViewById(R.id.btnLogin);
+        forgotPass = (TextView) findViewById(R.id.forgotPassword);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +46,71 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i1);
             }
         });
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forgotPassDia(MainActivity.this);
+            }
+        });
+    }
+
+    //method for forgot password dialog
+    public void forgotPassDia(Context context){
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
+        builder.setTitle("Forgot Password");
+        builder.setIcon(getDrawable(R.drawable.forgotpass_icon));
+        builder.setBackground(getDrawable(R.drawable.dialog_bg));
+        builder.setMessage("\nEnter registered email address.");
+
+        LinearLayout recov_layout = new LinearLayout(context);
+        recov_layout.setOrientation(LinearLayout.VERTICAL);
+        recov_layout.setVerticalGravity(10);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        params.setMargins(60,0,60,0);
+        EditText et_recovEmail = new EditText(context);
+        et_recovEmail.setBackground(getResources().getDrawable(R.drawable.tf_background));
+        et_recovEmail.setPadding(30,0,0,0);
+        et_recovEmail.setHint("Email Address");
+        et_recovEmail.setTextSize(15);
+        et_recovEmail.setLayoutParams(params);
+        et_recovEmail.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        recov_layout.addView(et_recovEmail);
+        builder.setView(recov_layout);
+
+        builder.setPositiveButton("SUBMIT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Codes here
+                accountRecovDia(context);
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //codes here
+            }
+        });
+        builder.show();
+    }
+
+    //method for Account Recovery Dialog
+    public void accountRecovDia(Context context){
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
+        builder.setTitle("Account Recovery");
+        builder.setIcon(R.drawable.accrecovery_icon);
+        builder.setBackground(getDrawable(R.drawable.dialog_bg));
+        builder.setMessage(getResources().getString(R.string.accRecovMsg));
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Codes here
+            }
+        });
+        builder.show();
     }
 }
