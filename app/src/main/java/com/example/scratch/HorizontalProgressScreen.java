f5d053d5f5bc4efbf56d7ad48d4b3cb1a0eefadc
charleology <cnbelano@gmail.com>
2022-08-12 14:08:44 +0800

    Tweaks on nav and process.

--- app/src/main/java/com/example/scratch/HorizontalProgressScreen.java
+++ app/src/main/java/com/example/scratch/HorizontalProgressScreen.java
@@ -3,13 +3,18 @@ package com.example.scratch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HorizontalProgressScreen extends AppCompatActivity {
+
+    TextView horizontalProgressTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horizontal_progress_screen);
         getSupportActionBar().hide();
+
+        horizontalProgressTv = (TextView) findViewById(R.id.horizontalProgressTv);
     }
 }
